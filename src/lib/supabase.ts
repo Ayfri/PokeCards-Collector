6d65ef3cc3db14bfb193f1bf5a0a20d0af9e47b5
Ayfri <pierre.ayfri@gmail.com>
2025-04-04 08:53:19 +0200

    refactor(supabase): Remove default values for Supabase.

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient, type SupabaseClient } from '@supabase/supabase-js';
 
 // Récupérer les valeurs des variables d'environnement
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://ptbwuqaqkqntmgmaqboq.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0Ynd1cWFxa3FudG1nbWFxYm9xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM1MDk2MDIsImV4cCI6MjA1OTA4NTYwMn0.zHklpHnOAQ1IvdKjoRnUTFIiPmSMiFiIN9_P0NNuqrg';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 // Créer le client Supabase
 let supabase: SupabaseClient;
