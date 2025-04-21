b23526d9b33e3dfae116cc3c9fe80bf34cf4b644
Ayfri <pierre.ayfri@gmail.com>
2025-04-21 02:35:05 +0200

    refactor(card): Add type annotations for card-related data in +page.svelte.

--- src/routes/card/[numero]/+page.svelte
+++ src/routes/card/[numero]/+page.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import CardDisplay from "@components/card/CardDisplay.svelte";
-
+	import type { Card, Pokemon, Set } from "$lib/types";
 	export let data: PageData;
 
-	$: cards = data.cards;
-	$: pokemons = data.pokemons;
-	$: sets = data.sets;
-	$: pokemon = data.pokemon;
+	$: cards = data.cards as Card[];
+	$: pokemons = data.pokemons as Pokemon[];
+	$: sets = data.sets as Set[];
+	$: pokemon = data.pokemon as Pokemon;
 </script>
 
 <main class="max-w-[100vw] p-2 text-lg text-white">
