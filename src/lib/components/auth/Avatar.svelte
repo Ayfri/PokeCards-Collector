571752f615198607a2543060d5a64c40c6d8afed
Ayfri <pierre.ayfri@gmail.com>
2025-04-22 23:56:14 +0200

    refactor(avatar): Update Avatar component background to a gradient for improved visual appeal..

--- src/lib/components/auth/Avatar.svelte
+++ src/lib/components/auth/Avatar.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div
-	class="size-{size} rounded-full bg-red-500 flex items-center justify-center text-white uppercase font-bold"
+	class="size-{size} rounded-full bg-gradient-to-tr from-red-500 to-purple-500 flex items-center justify-center text-white uppercase font-bold"
 	class:text-xl={size >= 12 && size < 16}
 	class:text-2xl={size >= 16 && size < 20}
 	class:text-3xl={size >= 20}
