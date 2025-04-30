f9bbaed92599b13b499e09d9a16c10ac5ae0af46
Ayfri <pierre.ayfri@gmail.com>
2025-04-30 10:26:21 +0200

    fix(scroll-to): Fix scroll-to buttons going over filters.

--- src/lib/components/list/ScrollToTop.svelte
+++ src/lib/components/list/ScrollToTop.svelte
@@ -1,5 +1,5 @@
 <button
-	class="fixed right-3 lg:right-6 bottom-20 xs:bottom-24 lg:bottom-24 size-12 z-50 opacity-50 border-white border-solid border-2 rounded-full bg-black fill-white p-2 cursor-pointer transition-all ease-in-out duration-300 hover:scale-110 hover:opacity-100"
+	class="fixed right-3 lg:right-6 bottom-20 xs:bottom-24 lg:bottom-24 size-12 z-30 opacity-50 border-white border-solid border-2 rounded-full bg-black fill-white p-2 cursor-pointer transition-all ease-in-out duration-300 hover:scale-110 hover:opacity-100"
 	id="scroll-top-btn"
 	on:click
 >
