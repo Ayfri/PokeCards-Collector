830f13554ddd538aaf73d4cb8243a267d39d26ba
Ayfri <pierre.ayfri@gmail.com>
2025-06-04 15:40:53 +0200

    refactor(card-page): Streamline card data loading by removing caching logic and directly fetching cards and pokemons; enhance clarity in Svelte component structure.

--- src/routes/card/[cardCode]/+page.svelte
+++ src/routes/card/[cardCode]/+page.svelte
@@ -2,29 +2,35 @@
 	import type { PageData } from './$types';
 	import CardDisplay from "@components/card/CardDisplay.svelte";
 	import type { Card, Pokemon, Set, PriceData } from "$lib/types";
-	import { fade } from 'svelte/transition';	
-	
+	import { fade } from 'svelte/transition';
+
 	export let data: PageData;
 
 	$: allCards = data.allCards as Card[];
 	$: pokemonCards = data.pokemonCards as Card[];
 	$: pokemons = data.pokemons as Pokemon[];
 	$: sets = data.sets as Set[];
-	$: pokemon = data.pokemon as Pokemon;
 	$: prices = data.prices as Record<string, PriceData>;
 	$: targetCard = data.targetCard as Card;
 </script>
 
 <main class="max-w-[100vw] p-4 mb-4 text-lg text-white">
 	<div class="mt-4 mx-auto flex flex-col gap-8 w-[90%] -z-10">
-		{#if !pokemonCards || pokemonCards.length === 0 || !targetCard}
+		{#if !pokemonCards?.length || !targetCard}
 			<p class="text-center text-lg">Chargement en cours...</p>
 		{:else}
 			{#key targetCard.cardCode}
 				<div in:fade={{ duration: 300 }}>
-					<CardDisplay allCards={allCards} pokemonCards={pokemonCards} {pokemons} {sets} {prices} lowRes={true} />
+					<CardDisplay
+						{allCards}
+						{pokemonCards}
+						{pokemons}
+						{sets}
+						{prices}
+						lowRes={true}
+					/>
 				</div>
 			{/key}
 		{/if}
 	</div>
-</main> 
+</main>
